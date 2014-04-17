54c88bfeb032bd06328d7e48e7a0a3ab97d5184b
Yalçın Ceylan <yalcinceylan93@gmail.com>
2014-04-17 23:11:20 +0300

    Create staticaccess.php

---
+++ staticaccess.php
@@ -0,0 +1,90 @@
+<?php
+
+/**
+ * StaticAccess Class for PHP OOP
+ * Creator: Yalçın CEYLAN
+ * Github: http://github.com/yali4/
+ * Website: http://yalcinceylan.net
+ * License: MIT <http://opensource.org/licenses/mit-license.php>
+ */
+class StaticAccess {
+
+    /**
+     * Örneği döndürmek için.
+     *
+     * @return mixed
+     */
+    public static function getInstance()
+    {
+
+        if ( !static::$instance )
+        {
+            static::$instance = new static();
+        }
+
+        return static::$instance;
+
+    }
+
+    /**
+     * Örnek üzerinden erişimler.
+     *
+     * @param $method
+     * @param array $args
+     * @return mixed
+     */
+    private static function callMethod($method, array $args)
+    {
+
+        $instance = self::getInstance();
+
+        $method = 'static'.ucfirst($method);
+
+        switch( count($args) )
+        {
+            case 0:
+                return $instance->$method();
+
+            case 1:
+                return $instance->$method($args[0]);
+
+            case 2:
+                return $instance->$method($args[0], $args[1]);
+
+            case 3:
+                return $instance->$method($args[0], $args[1], $args[2]);
+
+            case 4:
+                return $instance->$method($args[0], $args[1], $args[2], $args[3]);
+
+            default:
+                return call_user_func_array(array($instance, $method), $args);
+        }
+
+    }
+
+    /**
+     * Normal çağrılar için.
+     *
+     * @param $method
+     * @param $arguments
+     * @return mixed
+     */
+    public function __call($method, $arguments)
+    {
+        return self::callMethod($method, $arguments);
+    }
+
+    /**
+     * Statik çağrılar için.
+     *
+     * @param $method
+     * @param $arguments
+     * @return mixed
+     */
+    public static function __callStatic($method, $arguments)
+    {
+        return self::callMethod($method, $arguments);
+    }
+
+}
